68c803420cc7b9726bbe6707532139928ab93bbc
Peter Keenan <peter.keenan@sage.com>
2024-01-03 14:45:17 +0000

    Add basic tf project with basic deploy

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "core-tf-state-pk-test"
+    region         = "eu-west-1"
+    key            = "terraform.tfstate"
+    dynamodb_table = "core-tf-lock-dynamo-pk-test"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-pk-test"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "pk-test"
+  }
+}
